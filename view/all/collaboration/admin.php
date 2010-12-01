4ba0992711a0ba8727cbf652a165354b91b37eef
Marcel Balaz <marcelxbalaz@gmail.com>
2010-12-01 11:34:19 +0100

    Bugfix of previous refactoring, it was bad idea

--- view/all/collaboration/admin.php
+++ view/all/collaboration/admin.php
@@ -27,8 +27,8 @@
                         $name = $role['role'];
                         echo '<option value="' . $role['id'] . '"';
                         if ($collaboration_roles[$collaboration_user['id']] == $role['id'])
-                            echo ' selected="selected">';
-                        echo $name . '</option>';
+                            echo ' selected="selected"';
+                        echo '>' . $name . '</option>';
                     }
                     echo '</select></td>';
                     echo '<td align="center"><input type="checkbox" name="existing_user[' . $i . '][action][remove]"/></td></tr>';
@@ -62,8 +62,8 @@
                     $name = $role['role'];
                     echo '<option value="' . $role['id'] . '"';
                     if (2 == $role['id'])
-                        echo ' selected="selected">';
-                    echo $name . '</option>';
+                        echo ' selected="selected"';
+                    echo '>' . $name . '</option>';
                 }
                 echo '</select></td>';
                 echo '<td align="center"><input type="checkbox" name="new_user[' . $i . '][action][add]"/></td></tr>';
